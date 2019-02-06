0b9a5d45a82982804e80a84080fcd533ac792acb
Carl Hinderer <carl.hinderer4@gmail.com>
2019-02-06 15:07:20 -0700

    Finished slot machine game.

--- exercises/chapter03/slots.cpp
+++ exercises/chapter03/slots.cpp
@@ -3,34 +3,39 @@
  * ------------------ 
  * Plays a slot machine game.
  *
+ * Winning Combinations:
+ *   BAR     BAR     BAR         pays$250
+ *   BELL    BELL    BELL/BAR    pays$20
+ *   PLUM    PLUM    PLUM/BAR    pays$14
+ *   ORANGE  ORANGE  ORANGE/BAR  pays$10
+ *   CHERRY  CHERRY  CHERRY      pays$7
+ *   CHERRY  CHERRY  —           pays$5
+ *   CHERRY  —       —           pays$2
  */
 
-#include <cstdlib>
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include "random.h"
 using namespace std;
 
-/* Type definitions */
-struct winningCombinationT {
-    string wheel1;
-    string wheel2;
-    string wheel3;
-    int prize;
-};
-
 /* Constants */
 const int STARTING_DOLLARS = 20;
 const string SYMBOLS[] = {"CHERRY", "LEMON", "ORANGE", "PLUM", "BELL", "BAR"};
-const int numSymbols = 6;
+const int NUM_SYMBOLS = 6;
+const int SYMBOL_WIDTH = 9;
 
 
 /* Function Prototypes */
 void PlaySlots();
 int PullLever(int dollars);
+void PrintLeverPull(string wheel1, string wheel2, string wheel3);
 string SpinWheel();
 int GetResult(int dollars, string wheel1, string wheel2, string wheel3);
+int WinningResult(int dollars, int prize);
+int LosingResult(int dollars);
 bool PlayAgain(int dollars);
+bool UserWantsToPlay();
 
 
 /* Main Execution */
@@ -53,26 +58,75 @@ int PullLever(int dollars) {
     string wheel1 = SpinWheel();
     string wheel2 = SpinWheel();
     string wheel3 = SpinWheel();
+    PrintLeverPull(wheel1, wheel2, wheel3);
     return GetResult(dollars, wheel1, wheel2, wheel3);
 }
 
+void PrintLeverPull(string wheel1, string wheel2, string wheel3) {
+    cout << left
+         << setw(SYMBOL_WIDTH) << wheel1 
+         << setw(SYMBOL_WIDTH) << wheel2 
+         << setw(SYMBOL_WIDTH) << wheel3 << " -- ";
+}
+
 string SpinWheel() {
-    int symbolIndex = RandomInteger(1, numSymbols);
+    int symbolIndex = RandomInteger(0, NUM_SYMBOLS-1);
     return SYMBOLS[symbolIndex];
 }
 
 int GetResult(int dollars, string wheel1, string wheel2, string wheel3) {
-    cout << wheel1 << "  " << wheel2 << "  " << wheel3 << endl;
-    cout << "You lost." << endl;
-    return dollars - 1;
+    if (wheel1 == "BAR") {
+        if (wheel2 == "BAR" && wheel3 == "BAR") {
+            return WinningResult(dollars, 250);
+        }
+    }
+    else if (wheel1 == "BELL") {
+        if (wheel2 == "BELL" && (wheel3 == "BELL" || wheel3 == "BAR")) {
+            return WinningResult(dollars, 20);
+        }
+    }
+    else if (wheel1 == "PLUM") {
+        if (wheel2 == "PLUM" && (wheel3 == "PLUM" || wheel3 == "BAR")) {
+            return WinningResult(dollars, 14);
+        }
+    }
+    else if (wheel1 == "ORANGE") {
+        if (wheel2 == "ORANGE" && (wheel3 == "ORANGE" || wheel3 == "BAR")) {
+            return WinningResult(dollars, 10);
+        }
+    }
+    else if (wheel1 == "CHERRY") {
+        int prize = 2;
+        if (wheel2 == "CHERRY") {
+            prize += 3;
+            if (wheel3 == "CHERRY") prize += 2;
+        }
+        return WinningResult(dollars, prize);
+    }
+
+    return LosingResult(dollars);
+}
+
+int WinningResult(int dollars, int prize) {
+    cout << "You win $" << prize << "." << endl;
+    return dollars + prize;
+}
+
+int LosingResult(int dollars) {
+    cout << "You lose." << endl;
+    return dollars-1;
 }
 
 bool PlayAgain(int dollars) {
     cout << "Dollars: " << dollars << endl;
     if (dollars <= 0) return false;
+    return UserWantsToPlay();
+}
+
+bool UserWantsToPlay() {
     while(true) {
         string input;
-        cout << "Would you like to pull the lever? Enter 'Y' or 'N'." << endl;
+        cout << "Would you like to pull the lever? Enter 'Y' or 'N'. ";
         cin >> input;
         if (input == "Y") return true;
         if (input == "N") return false;
